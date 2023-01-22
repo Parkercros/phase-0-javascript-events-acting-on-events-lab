064d9a6cd25321ed05167699c115b2f51c2c2a1d
Parker Crosgrove <pdxparker3@gmail.com>
2023-01-21 20:12:36 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left);
+    left -= 10;
+    if(left>=0)
+    dodger.style.left = left + 'px';
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left);
+    left += 10;
+    if(left<=window.innerWidth)
+    dodger.style.left = left + 'px';
+  }
+  
